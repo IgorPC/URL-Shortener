1f27aec6a67e4be1576e08bbd08bdaf8a6d0e168
Igor Coutinho <igorpc.tv@gmail.com>
2026-02-11 22:55:07 -0300

    created initial structure

---
+++ api/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! -d "vendor" ]; then
+    composer install --no-interaction --optimize-autoloader
+fi
+
+echo "Waiting project to start..."
+
+sleep 10
+
+php artisan migrate --force
+
+php artisan config:clear
+php artisan route:clear
+
+echo "Starting queues..."
+php artisan queue:work --verbose --tries=3 --timeout=90 &
+
+echo "Running server..."
+php artisan serve --host=0.0.0.0 --port=8000
